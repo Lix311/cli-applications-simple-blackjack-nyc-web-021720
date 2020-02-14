ec7883548d8b4455995092a7ea780f2b32d335ad
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-14 04:48:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -83,9 +83,9 @@ def runner
   welcome
   initial_round
   players_card_total = initial_round
-  binding.pry 
+
   until players_card_total > 21
-    hit?(players_card_total)
+    
     players_card_total += hit?(players_card_total)
     display_card_total(players_card_total)
   end 
